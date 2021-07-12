6910d8869d9f2f618a9e656f2e0d411e45edb696
Amey Singh <amay.chauhan@gmail.com>
2021-07-12 08:39:30 +0530

    12 July

---
+++ concepts/aws_analytics/s3util/s3FileUpload.py
@@ -0,0 +1,31 @@
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+
+def upload_file(file_name, bucket, object_name=None):
+
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3',
+                         aws_access_key_id="AKIA4DZ4H26U2LLDGYPR",
+                         aws_secret_access_key="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")
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+for i in range(10):
+    print("Uploading file", i)
+    obj_name = 'click-data/Clicks'+str(i)+'.json'
+    upload_file('Clicks.json','ameytestbucket',obj_name)
+
+
+
+
