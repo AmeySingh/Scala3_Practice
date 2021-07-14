3b10522e4532e13e0b9211dcc4e31b0e66956875
Amey Singh <amay.chauhan@gmail.com>
2021-07-14 13:37:47 +0530

    "Commit 14 July"

--- concepts/aws_analytics/s3util/s3MainApp.py
+++ concepts/aws_analytics/s3util/s3MainApp.py
@@ -2,13 +2,9 @@ import boto3
 import boto3.s3
 
 s3r = boto3.resource('s3',
-    aws_access_key_id="AKIA4DZ4H26U2LLDGYPR",
-    aws_secret_access_key="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",
+    aws_access_key_id="AKIA4DZ4H26UUOSO67GF",
+    aws_secret_access_key="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",
 )
-
-for bucket in s3r.buckets.all():
-    for key in bucket.objects.all():
-        print(key.key)
 
 s3c = boto3.client('s3',
     aws_access_key_id="AKIA4DZ4H26U2LLDGYPR",
